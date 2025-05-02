71b7f659ba1accf0cce44aef5ebdddb3e7522b52
清水博人 <shimizuhiroto@shimizuhirotonoMacBook-Air.local>
2025-05-03 01:16:07 +0900

    done2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '../generated/prisma';
 import cors from "cors";
 
 const app : Express = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
